93111d2514a3f3b3f13cd2a06fdc825965188299
Hyunjae Lee <guswoqkrtk@gmail.com>
2022-04-11 23:37:13 +0900

    ChallengeCardView 수정

--- EZPZ_ZZIN/EZPZ_ZZIN/ChallengeCardView.swift
+++ EZPZ_ZZIN/EZPZ_ZZIN/ChallengeCardView.swift
@@ -12,9 +12,31 @@ struct ChallengeCardView: View {
     @ObservedObject var challengeEntity: ChallengeEntity
     
     func getRatio() -> Double {
-        let numerator = challengeEntity.start!.distance(to: Date())
-        let denominator = challengeEntity.start!.distance(to: challengeEntity.end!)
-        return numerator / denominator
+        guard let startDate = challengeEntity.start else {
+            print("도전 시작 날짜가 지정되어 있지 않음")
+            return 0.0
+        }
+        guard let endDate = challengeEntity.end else {
+            print("도전 종료 날짜가 지정되어 있지 않음")
+            return 0.0
+        }
+        let currentDate: Date = Date()
+        let numerator = startDate.distance(to: currentDate)
+        let denominator = startDate.distance(to: endDate)
+        
+        // 0으로 나누는 경우가 생기지 않도록 예외 처리
+        if denominator == 0 {
+            return 1.0
+        }
+        var ratio: Double = numerator / denominator
+        if ratio < 0.0 {
+            // 0보다 작은 경우 0으로 처리
+            ratio = 0.0
+        } else if ratio > 1.0 {
+            // 1보다 큰 경우 1로 처리
+            ratio = 1.0
+        }
+        return ratio
     }
     
     func getPercentage() -> Int {
