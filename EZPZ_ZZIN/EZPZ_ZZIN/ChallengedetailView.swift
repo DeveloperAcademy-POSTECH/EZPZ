474f9401b9dc8b74ab0096882ea6c0de38887153
Juhwa-Lee1023 <63584245+Juhwa-Lee1023@users.noreply.github.com>
2022-04-12 15:38:52 +0900

    Challenge detail view update
    2nd

--- EZPZ_ZZIN/EZPZ_ZZIN/ChallengedetailView.swift
+++ EZPZ_ZZIN/EZPZ_ZZIN/ChallengedetailView.swift
@@ -15,6 +15,8 @@ struct ChallengedetailView: View {
     @State var fifthCheck = false
     @State var sixthCheck = false
     @State var checkBool = false
+    @State private var showingActionSheet = false
+    @State private var showAlert = false
     
     var body: some View {
         ZStack{
@@ -35,6 +37,26 @@ struct ChallengedetailView: View {
                         .foregroundColor(ColorManage.ezpzLightgrey)
                         .multilineTextAlignment(.leading).padding([.leading], 17).padding(.bottom,2)
                             Spacer()
+                            Button(action: {self.showingActionSheet.toggle()}) {
+                                Image(systemName: "gearshape")
+                                    .padding(.trailing, 17.0)
+                                    .foregroundColor(ColorManage.ezpzLightgrey)
+                                }
+                            .confirmationDialog(
+                                "도전명 변경 및 도전 기간을 수정할 수 있어요!",
+                                isPresented: $showingActionSheet,
+                                actions: {
+                                    Button("도전 수정하기") {
+                                        
+                                    }
+                                    Button("도전 삭제하기") {
+                                        showAlert = true
+                                    }
+                                    Button("Cancel", role: .cancel) { }
+                                })
+                            .alert(isPresented: $showAlert) {
+                                        Alert(title: Text("할 일을 삭제하시겠어요?"), message: Text("한 번 지운 할 일은 복구할 수 없어요..."), dismissButton: .default(Text("삭제하기")))
+                                    }
                         }
                         HStack{
                             Text("2022.03.12~2022.05.30")
@@ -71,6 +93,7 @@ struct ChallengedetailView: View {
                         Divider()
                             .background(ColorManage.ezpzLightgrey)
                         ForEach(1..<7) { i in
+                            
                         CheckboxField(id: "사이클 30분 타기", label: "사이클 30분 타기", isMarked: $firstCheck).padding(.leading , 17).padding([.top, .bottom], 6)
                         Divider()
                             .background(ColorManage.ezpzSmokegrey)
