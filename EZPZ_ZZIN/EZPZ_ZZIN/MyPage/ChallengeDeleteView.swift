e6331404dcb44e8e0a3b7b0ff9fb453b02b0232a
Hongjaaa <ghdwns516@naver.com>
2022-04-12 20:48:50 +0900

    마이페이지 패딩값 조정

--- EZPZ_ZZIN/EZPZ_ZZIN/MyPage/ChallengeDeleteView.swift
+++ EZPZ_ZZIN/EZPZ_ZZIN/MyPage/ChallengeDeleteView.swift
@@ -12,31 +12,45 @@ struct ChallengeDeleteView: View {
     @Environment(\.managedObjectContext) private var viewContext
     @FetchRequest(sortDescriptors: [NSSortDescriptor(keyPath: \ChallengeEntity.timestamp, ascending: true)])
     private var items: FetchedResults<ChallengeEntity>
+    @Environment(\.presentationMode) var presentationMode: Binding<PresentationMode>
     
     var body: some View {
         ZStack {
             Color("ezpzBlack")
                 .edgesIgnoringSafeArea(.all)
+            VStack {
+                HStack {
+                    Spacer()
+                    Button(action: {
+                        self.presentationMode.wrappedValue.dismiss()
+                        }) {
+                            Image(systemName: "xmark")
+                                .padding(.trailing, 30.0)
+                                .padding(.top, 60.0)
+                                .foregroundColor(ColorManage.ezpzLightgrey)
+                            }
+                    }
             ScrollView {
                 HStack {
                     Text("포기할 도전을 선택해주세요!")
                         .font(.system(size: 18))
                         .foregroundColor(Color("ezpzLightgrey"))
-                        .padding(.leading, 10)
+                        .padding(.leading, 30)
                     Spacer()
                 }
-                .padding(.top, 20)
-                CustomDividerView()
+            .padding(.top, 20)
+            CustomDividerView()
                 ForEach(items) { challengeEntity in
                     HStack {
                         Text("\(challengeEntity.emoji ?? "") \(challengeEntity.title ?? "")")
                             .font(.system(size: 18))
                             .fontWeight(.bold)
                             .foregroundColor(Color("ezpzLime"))
-                            .padding(.leading, 15)
+                            .padding(.leading, 30)
                         Spacer()
                     }
                     CustomDividerView()
+                    }
                 }
             }
         }
--- EZPZ_ZZIN/EZPZ_ZZIN/MyPage/ChangeNameView.swift
+++ EZPZ_ZZIN/EZPZ_ZZIN/MyPage/ChangeNameView.swift
@@ -12,22 +12,34 @@ struct ChangeNameView: View {
     @Environment(\.managedObjectContext) private var viewContext
     @FetchRequest(sortDescriptors: [NSSortDescriptor(keyPath: \ChallengeEntity.timestamp, ascending: true)])
     private var items: FetchedResults<ChallengeEntity>
-    @State private var checkAmount = ""
+    @Environment(\.presentationMode) var presentationMode: Binding<PresentationMode>
     
     var body: some View {
         ZStack {
             Color("ezpzBlack")
                 .edgesIgnoringSafeArea(.all)
+            VStack {
+                HStack {
+                    Spacer()
+                    Button(action: {
+                        self.presentationMode.wrappedValue.dismiss()
+                        }) {
+                            Image(systemName: "xmark")
+                                .padding(.trailing, 30.0)
+                                .padding(.top, 60.0)
+                                .foregroundColor(ColorManage.ezpzLightgrey)
+                            }
+                    }
             ScrollView {
                 HStack {
                     Text("닉네임을 변경해주세요!")
                         .font(.system(size: 18))
                         .foregroundColor(Color("ezpzLightgrey"))
-                        .padding(.leading, 10)
+                        .padding(.leading, 30)
                     Spacer()
                 }
-                .padding(.top, 20)
-                CustomDividerView()
+            .padding(.top, 20)
+            CustomDividerView()
                 ForEach(items) { challengeEntity in
                     VStack {
                         HStack {
@@ -35,11 +47,12 @@ struct ChangeNameView: View {
                             .font(.system(size: 18))
                             .fontWeight(.bold)
                             .foregroundColor(Color("ezpzLime"))
-                            .padding(.leading, 15)
+                            .padding(.leading, 30)
                         Spacer()
                         }
                     }
                     CustomDividerView()
+                    }
                 }
             }
         }
