85539e1ab77bf04a453a0cbb40c8f16db43c766b
Hyunjae Lee <guswoqkrtk@gmail.com>
2022-04-14 22:44:53 +0900

    템플릿 추천 알고리즘 구현

--- EZPZ_ZZIN/EZPZ_ZZIN/Common/CommonDetailView.swift
+++ EZPZ_ZZIN/EZPZ_ZZIN/Common/CommonDetailView.swift
@@ -398,22 +398,46 @@ struct TemplateView: View {
     }
 }
 
+struct Template {
+    let mainTitle: String
+    let title: String
+    let categoryInform: String
+    let todoRows: [String]
+}
 
+var A: [Template] = [
+    Template(mainTitle: "👍 덤벨 하나 덜어내기", title: "10kg 빼기", categoryInform: "카테고리 운동", todoRows: ["하루 한 끼 샐러드", "저녁 유산소 30분씩 하기", "매일 맨몸운동 3세트씩 하기", "매일 아침 공복 몸무게 재기"]),
+    Template(mainTitle: "👍 이제는 미룰 수 없다", title: "매일 하는 영어 공부", categoryInform: "카테고리 공부", todoRows: ["영단어 20개 외우기", "영어 기사 읽기", "BBC 뉴스 1개 보기"]),
+    Template(mainTitle: "👍 일찍 일어나는 새가 으스댄다", title: "얼리버드의 삶", categoryInform: "카테고리 공부", todoRows: ["매일 5시 기상", "조간신문 읽기", "해 뜨는 것 사진찍기"]),
+]
+
+var B: [Template] = [
+    Template(mainTitle: "👍 30만원의 행복", title: "한달 식비 30만원으로 살아보기", categoryInform: "카테고리 습관", todoRows: ["장보기", "하루에 집밥 2번 먹기", "가계부 쓰기"]),
+    Template(mainTitle: "👍 젊은 날의 기록", title: "바디프로필 촬영", categoryInform: "카테고리 운동", todoRows: ["헬스장 가기", "단백질 보충제 먹기", "잠 일찍 자기(10시)", "스튜디오 상담"]),
+    Template(mainTitle: "👍 내일은 나도 피카소", title: "드로잉 배우기", categoryInform: "카테고리 공부", todoRows: ["하루에 크로키 3개", "크로키 영상 보기", "인스타에 그린 것 올리기", "연필 깍기"]),
+]
+
+var C: [Template] = [
+    Template(mainTitle: "👍 이제는 챙길 나이", title: "건강 챙기기", categoryInform: "카테고리 건강", todoRows: ["매일 영양제", "아침 따뜻한 물", "허리 핏자"]),
+    Template(mainTitle: "👍 공인중개사 합격은...?", title: "공인중개사 자격 취득하기", categoryInform: "카테고리 공부", todoRows: ["하루에 기출 20문제", "하루에 인강 1강 보기", "인강 끊기"]),
+    Template(mainTitle: "👍 쾌적한 삶의 시작", title: "정리하기", categoryInform: "카테고리 습관", todoRows: ["아침 이부자리 정리", "저녁 내일 일정 확인하기", "책상 정리하기", "방 청소하기"]),
+]
+
+var D: [Template] = [
+    Template(mainTitle: "👍 10분만 걸어도 숨이 차요 ㅠㅠ", title: "일상속의 운동", categoryInform: "카테고리 운동", todoRows: ["출퇴근, 등교시 에스컬레이터 대신 계단을 이용해요", "아침 먹기전 10분씩 바닥에서 스트레칭을 해봐요", "스트레칭 유튜브 영상 하나 보기"]),
+    Template(mainTitle: "👍 아~ 센델형~", title: "’정의란 무엇인가' 읽기", categoryInform: "카테고리 공부", todoRows: ["매일 20분 읽기", "한 줄 독후감", "독서 모임 나가기 "]),
+    Template(mainTitle: "👍 #관리하는남자", title: "놓칠 수도 있는 것", categoryInform: "카테고리 습관", todoRows: ["집 밖에 나가기 전 선크림 바르기", "세수할 때, 눈썹 정리하기", "자기 전 손, 발톱 확인하기", "콧털 정리하기"]),
+]
 
 // 템플릿 추천 받기
 
 struct CommonChallengeTemplateView: View {
+    
     var allString = "도전하고싶은\n템플릿을 골라주세요"
     var partialString = "템플릿"
     @Binding var challenge: String
     @Binding var pageNum : Int
-    
-    // example
-    var Templates: [TemplateView] = [
-        TemplateView(mainTitle: "👍 멘탈 케어 템플릿", title: "마음이 건강", categoryInform: "카테고리 건강", todoRows: ["저녁 먹고 차 마시기", "자기 전에 명상하기", "아침에 출근하면서 노래 듣기"]),
-        TemplateView(mainTitle: "👍 멘탈 케어 템플릿", title: "마음이 건강", categoryInform: "카테고리 건강", todoRows: ["저녁 먹고 차 마시기", "자기 전에 명상하기", "아침에 출근하면서 노래 듣기"]),
-        TemplateView(mainTitle: "👍 멘탈 케어 템플릿", title: "마음이 건강", categoryInform: "카테고리 건강", todoRows: ["저녁 먹고 차 마시기", "자기 전에 명상하기", "아침에 출근하면서 노래 듣기"])
-    ]
+    @Binding var accumulativeSum: Int
     
     var body: some View {
         let mainText = partialColorString(allString: allString, allStringColor: .white, partialString: partialString, partialStringColor: Color("ezpzLime"))
@@ -437,15 +461,34 @@ struct CommonChallengeTemplateView: View {
                 ScrollView(.horizontal, showsIndicators: false) {
                     HStack(spacing: 10) {
                         
-                        ForEach(0..<Templates.count) { i in
-                            Templates[i]
-                                .onTapGesture {
-                                    withAnimation {
-                                        challenge = Templates[i].title
-                                        pageNum += 1
+                        if 0 <= accumulativeSum && accumulativeSum <= 6 {
+                            ForEach(A, id: \.title) { template in
+                                TemplateView(mainTitle: template.mainTitle, title: template.title, categoryInform: template.categoryInform, todoRows: template.todoRows)
+                                    .onTapGesture {
+                                        print(accumulativeSum)
                                     }
-                                }
-                                
+                            }
+                        } else if 7 <= accumulativeSum && accumulativeSum <= 12 {
+                            ForEach(B, id: \.title) { template in
+                                TemplateView(mainTitle: template.mainTitle, title: template.title, categoryInform: template.categoryInform, todoRows: template.todoRows)
+                                    .onTapGesture {
+                                        print(accumulativeSum)
+                                    }
+                            }
+                        } else if 13 <= accumulativeSum && accumulativeSum <= 18 {
+                            ForEach(C, id: \.title) { template in
+                                TemplateView(mainTitle: template.mainTitle, title: template.title, categoryInform: template.categoryInform, todoRows: template.todoRows)
+                                    .onTapGesture {
+                                        print(accumulativeSum)
+                                    }
+                            }
+                        } else if 19 <= accumulativeSum && accumulativeSum <= 24 {
+                            ForEach(D, id: \.title) { template in
+                                TemplateView(mainTitle: template.mainTitle, title: template.title, categoryInform: template.categoryInform, todoRows: template.todoRows)
+                                    .onTapGesture {
+                                        print(accumulativeSum)
+                                    }
+                            }
                         }
                             
                     }.padding(.leading, 15)
@@ -459,9 +502,6 @@ struct CommonChallengeTemplateView: View {
         .background(Color("ezpzBlack"))
             
     }
-        
-    
-    
 }
 
 
